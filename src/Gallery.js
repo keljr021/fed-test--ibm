4edee754853b7abaf8e62bffd0cba6faf884d84b
Kelvin Morrisey <keljr021@gmail.com>
2019-07-23 20:25:26 -0400

    Modified grid for Gallery.

--- src/Gallery.js
+++ src/Gallery.js
@@ -11,11 +11,11 @@ class Gallery extends Component {
                 <div className="gallery-title mdl-cell mdl-cell--12-col-desktop">
                     <h4>Gallery</h4>
                 </div>
-                <div className="gallery-text mdl-cell mdl-cell--6-col-desktop">
+                <div className="gallery-text mdl-cell mdl-cell--4-col-desktop">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquet luctus est nec vehicula. Sed id tellus hendrerit, condimentum dolor quis, feugiat ligula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut suscipit eros ipsum, vel suscipit metus laoreet vitae. Pellentesque sodales faucibus ultrices. Donec auctor varius purus. Aenean pellentesque non lacus quis fermentum.</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquet luctus est nec vehicula. Sed id tellus hendrerit, condimentum dolor quis, feugiat ligula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut suscipit eros ipsum, vel suscipit metus laoreet vitae. Pellentesque sodales faucibus ultrices. Donec auctor varius purus. Aenean pellentesque non lacus quis fermentum.</p>
                 </div>
-                <div className="gallery-images mdl-cell mdl-cell--6-col-desktop">
+                <div className="gallery-images mdl-cell mdl-cell--8-col-desktop">
                     <img alt="#" src="./images/stranger-things_raw.png" />
                 </div>                     
             </div>
